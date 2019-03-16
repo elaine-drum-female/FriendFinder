02086186a484a12ed7e096f0f2bdc4505de3d145
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-15 21:24:00 -0500

    Added friends.js file to the app/data folder

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+var friendsArray =[ {
+        "name":"Ahmed",
+        "photo":"https://images.unsplash.com/photo-1550051414-003c9007794c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Elaine",
+        "photo":"https://github.com/elaine-drum-female/new-portfolio-site/blob/master/assets/imgs/me.jpg",
+        "scores":[
+            3,
+            4,
+            2,
+            1,
+            4,
+            1,
+            3,
+            2,
+            1,
+            1
+          ]
+      },
+
+      {
+        "name":"Carol",
+        "photo":"https://images.unsplash.com/photo-1536795247995-0576c1de8ddd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+        "scores":[
+            2,
+            1,
+            4,
+            4,
+            3,
+            3,
+            4,
+            5,
+            1,
+            2
+          ]
+      },
+
+      {
+        "name":"Little ZooLu",
+        "photo":"https://images.unsplash.com/photo-1549881349-017011b6122e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "scores":[
+            1,
+            3,
+            2,
+            1,
+            2,
+            3,
+            1,
+            4,
+            2,
+            2
+          ]
+      }
+
+      ]
+      
+module.exports = friendsArray;
